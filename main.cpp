5ebc486fd327adfdd33d9219a24618d8a5cacd3b
Damian Dragovoz <damiandragovoz@gmail.com>
2025-06-07 16:45:06 +0300

    Added second thread. Now need to add command queue

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
-#include <thread>
 #include <nlohmann/json.hpp>
-#include <assert.h>
 #include "Functions_file_system.h"
 #include "Functions_networking.h"
 #include "Functions_win32.h"
@@ -10,21 +8,13 @@
 #include <fstream>
 #include <iostream>
 #include <filesystem>
+#include <assert.h>
 #include <vector>
 #include <string>
+#include <thread>
 
-// NOTE(damian): Messages have the following strcture:
-//				 <command> <args1> <arg2> ... 
-
-// NOTE(damian): 
-// 		Messages that are currently supported:
-//			-- (report)   --> get complete data
-//			-- (quit)	  --> disconnect the current client
-//			-- (shutdown) --> stop the tracking completely
-//			-- (track)    --> add a process to be tracked
-// 			-- (untrack)  --> remove a process from the tracking list
-
-void split_std_string(std::string* str, char delimeter, std::vector<std::string>* sub_strings);
+void data_thread();
+void client_thread();
 
 void wait_for_client_to_connect();
 
@@ -38,40 +28,44 @@ void handle_untrack (std::string* path);
 static bool   running = true;
 static SOCKET client_socket;
 static bool need_new_client = true;
-// static vector<string> message_parts;
-
-
-
-// int main() {
-//     Parser parser = parser_init(" pclarity track \"C:\\Users\\Admin\\AppData\\Roaming\\Telegram Desktop\\Telegram.exe\" deadas");
-//     pair<Command, bool> result = start_parsing(&parser);
-
-//     if (result.second == true) {
-//         std::cout << result.first.track.path_token.lexeme << std::endl;
-//     }
-//     else {
-//         std::cout << "invalid" << std::endl;
-//     }
-
-//     return 0;
-// }
-
-
 
 int main() {
 
  	G_state::set_up_on_startup();
- 	std::cout << "Done setting up. \n" << std::endl;
+	G_state::update_state();
+	std::cout << "Done setting up. \n" << std::endl;
 
- 	while (running) {
+	std::thread client (&client_thread); // This starts right away.
 
- 		if (need_new_client) {
+	while (running) {
+		std::chrono::duration<int> sleep_length(3);
+		std::this_thread::sleep_for(sleep_length);
+	}
+
+	return 0;
+}
+
+// TODO(damian): handle out of bounds for message_parts. 
+
+void data_thread() {
+	// This just gets the data. 
+}
+
+// TOOD(damian): think about namespacing function for both thread,
+//				 just to be more sure, that thread use shated data as little as possible.  
+
+void client_thread() {
+	while (running) {
+		if (need_new_client) {
  			wait_for_client_to_connect();
  			need_new_client = false;
  		}
+
+		// NOTE: dont forget to maybe extend it if needed, or error if the message is to long or something.
+ 		// NOTE(damian): recv doesnt null terminate the string buffer, 
+ 		//	             so terminating it myself, to then be able to use strcmp.
 		
- 		// NOTE: dont forget to maybe extend it if needed, or error if the message is to long or something.
- 		char receive_buffer[512];
+		char receive_buffer[512];
  		int  receive_buffer_len = 512;
 
  		int n_bytes_returned = recv(client_socket, receive_buffer, receive_buffer_len, NULL); // TODO(damian): add a timeout here.
@@ -80,90 +74,74 @@ int main() {
  		std::cout << "Received a message of " << n_bytes_returned << " bytes." << std::endl;
  		std::cout << "Message: " << "'" << receive_buffer << "'" << "\n" << std::endl;
 
- 		G_state::update_state();
-	    // TODO(damian): Would like a switch more here.		
-
- 		// NOTE(damian): recv doesnt null terminate the string buffer, 
- 		//	     so terminating it myself, to then be able to use strcmp.
-
- 		// TODO(damian): this might over_flow, handle it, just in case.
-
- 		// TODO(damian): create separate handlers for messages. Code would be cleaner. 
-
- 		// string message_as_std_string = receive_buffer;
- 		// split_std_string(&message_as_std_string, ' ', &message_parts);
-
-        // Parsing the request.
+		// Parsing the request.
 		json j_message = json::parse(receive_buffer);
-		if (   j_message.contains("command_id")
-			&& j_message.contains("extra")) 
-		{
-			int command_id = j_message["command_id"];
-			std::optional<Command> opt = command_from_int(command_id);
-			
-			if (!opt.has_value()) {
-				string message = "Invalid command.";
-				int send_err_code = send(client_socket, message.c_str(), message.length(), NULL);
-				if (send_err_code == SOCKET_ERROR) {
-					// TODO: handle
-				}
-			}
+		
+		const char* invalid_command_message = nullptr;
 
-			const char* invalid_command_message = nullptr;
-
-			Command command = opt.value();
-			switch (command) {
-                case Command::report: {
-                    handle_report();
-                } break;
-                
-                case Command::quit: {
-                    handle_quit();
-                } break;
-
-                case Command::shutdown: {
-                    handle_shutdown();
-                } break;
-
-                case Command::track: {
-					json j_extra = j_message["extra"];
-					if (!j_extra.contains("path")) {
-						invalid_command_message = "Invalid command. Track has to have path inside extra.";
-						break;
-					}
-					string path = j_extra["path"];
-
-                    handle_track(&path);
-                } break;
-
-                case Command::untrack: {
-                    json j_extra = j_message["extra"];
-					if (!j_extra.contains("path")) {
-						invalid_command_message = "Invalid command. Untrack has to have path inside extra.";
-						break;
-					}
-					string path = j_extra["path"];
-
-                    handle_untrack(&path);
-                } break;
-
-                default: {
-                    assert(false);
-                } break;
-        
-            }
-
-			if (invalid_command_message != nullptr) {
-				int send_err_code = send(client_socket, invalid_command_message, strlen(invalid_command_message), NULL);
-				if (send_err_code == SOCKET_ERROR) {
-					// TODO: handle
-				}
-			}
-
-
+		if (   !j_message.contains("command_id")
+			|| !j_message.contains("extra")) 
+		{ 
+			invalid_command_message = "Invalid message structure."; 
 		}
-	 		
- 		// Preserve the new state
+
+		int command_id = j_message["command_id"];
+		std::optional<Command> opt = command_from_int(command_id);
+		
+		if (!opt.has_value()) {
+			invalid_command_message = "Invalid command.";
+		}
+
+		Command command = opt.value();
+		switch (command) {
+			case Command::report: {
+				handle_report();
+			} break;
+			
+			case Command::quit: {
+				handle_quit();
+			} break;
+
+			case Command::shutdown: {
+				handle_shutdown();
+			} break;
+
+			// case Command::track: {
+			// 	json j_extra = j_message["extra"];
+			// 	if (!j_extra.contains("path")) {
+			// 		invalid_command_message = "Invalid command. Track has to have path inside extra.";
+			// 		break;
+			// 	}
+			// 	string path = j_extra["path"];
+
+			// 	handle_track(&path);
+			// } break;
+
+			// case Command::untrack: {
+			// 	json j_extra = j_message["extra"];
+			// 	if (!j_extra.contains("path")) {
+			// 		invalid_command_message = "Invalid command. Untrack has to have path inside extra.";
+			// 		break;
+			// 	}
+			// 	string path = j_extra["path"];
+
+			// 	handle_untrack(&path);
+			// } break;
+
+			default: {
+				assert(false);
+			} break;
+	
+		}
+
+		if (invalid_command_message != nullptr) {
+			int send_err_code = send(client_socket, invalid_command_message, strlen(invalid_command_message), NULL);
+			if (send_err_code == SOCKET_ERROR) {
+				// TODO: handle
+			}
+		}
+
+		// Preserve the new state
  		std::ofstream data_file("data.json");
 			
  		vector<json> processes_as_jsons;
@@ -180,34 +158,6 @@ int main() {
 
  		std::cout << "Saved data to file. \n" << std::endl;
 
- 	}
-
-	return 0;
-}
-
-// TODO(damian): handle out of bounds for message_parts. 
-// TODO(damian): add something that will represent the expectations for the message command, then it will be responcible for message validity checks.
-
-void split_std_string(std::string* str, char delimeter, std::vector<std::string>* sub_strings) {
-	if (str->empty()) return;
-
-	bool have_seen_non_space_char = false;
-
-	size_t start_idx = 0;
-	for (int i = 0; i < str->length(); ++i) {
-		if (str->c_str()[i] == delimeter) {
-			std::string sub_string(str->c_str() + start_idx, i - start_idx);
-			sub_strings->push_back(sub_string);
-			start_idx = i + 1;
-		}
-		else {
-			have_seen_non_space_char = true;
-		}
-	}
-
-	if (have_seen_non_space_char) {
-		std::string sub_string(str->c_str() + start_idx, str->length() - start_idx);
-		sub_strings->push_back(sub_string);
 	}
 
 }
